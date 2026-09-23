e4f1e65cd6218867de0340a2b23cc199a4e07392
Your Name <Your Name>
2026-09-23 21:15:03 +0300

    first app

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 
-
-
+#include <stdio.h>
 
 int main()
+
 {
+int a = 0x77 , b = -20;
+char c = 'F';
+float f = 12.2341524;
+double d = 2e8;
+const char* string ="\n hello, world!";
+printf ("%s \n", string);
+printf("a = %d, b = %d\n",a ,b);
+printf("a = %u, b = %u\n",a ,b);
+printf("a = %x, b = %X\n",a ,b);
+printf("dec a = %d, oct a = %o, hex a = %x\n", a, a, a);
+printf(" floating point f = %f, exp f = %e\n", f, f);
+printf(" double d = %f or %E\n", d, d);
+printf("not all compiler suport %a\n", f);
+printf("character c = %c, as numer c = %d\n", c, c);
+printf("address of string is %p\n", string);
+    // getc();
+
     return 0;
 }
