ad4d3c7fac3cefbc4fbc1111fe59b50aeb76b7a0
Your Name <Your Name>
2026-09-23 21:18:09 +0300

    add labs

---
+++ src/lab2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+//    a = 20;
+//    b = 40
+//    c = 10
+//    double_float = a + b + c/3; 'prysvoyenya funkcii double_float znachenya a b c'
+
+
+
+//    scanf("%f", &double_float); 'vvod znachenya double_float'
+
+//    printf("%f \n", double_float); 'vyvid znachenya double_float'
+
+
+    return 0;
+}
